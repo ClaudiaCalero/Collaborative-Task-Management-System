15f34e680bda3a5af13c2757290e4b2706ed6095
ClaudiaCalero <claudiaclrduro@gmail.com>
2024-06-08 11:30:32 +0200

    Initial commit

---
+++ backend/src/main/java/com/project/backend/CollaborativeTaskManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.project.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollaborativeTaskManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollaborativeTaskManagementSystemApplication.class, args);
+	}
+
+}
