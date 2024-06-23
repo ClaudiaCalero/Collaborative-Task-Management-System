1cf14685b0dd5202238b6913bf77bf74ba88cb5a
ClaudiaCalero <claudiaclrduro@gmail.com>
2024-06-23 12:19:39 +0200

    taskController methods

--- backend/src/main/java/com/project/backend/controllers/TaskController.java
+++ backend/src/main/java/com/project/backend/controllers/TaskController.java
@@ -1,11 +1,43 @@
 package com.project.backend.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.project.backend.entities.Task;
+import com.project.backend.services.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/tasks")
 public class TaskController {
     //    //TaskController.java: Gestiona las solicitudes relacionadas con la gestión de tareas
     //    (crear, leer, actualizar y eliminar tareas).
+    @Autowired
+    private TaskService taskService;
+
+    @PostMapping
+    public Task createTask(@RequestBody Task task){
+        return taskService.createTask(task);
+    }
+
+    @GetMapping
+    public List<Task> getAllTasks() {
+        return taskService.getAllTasks();
+    }
+
+    @GetMapping("/{id}")
+    public Task getTaskById(@PathVariable Long id){
+        return taskService.getTaskById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Task updateTask(@PathVariable Long id, @RequestBody Task task){
+        return taskService.updateTask(id, task);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteTask(@PathVariable Long id){
+        taskService.deleteTask(id);
+    }
+
 }
--- backend/src/main/java/com/project/backend/services/TaskService.java
+++ backend/src/main/java/com/project/backend/services/TaskService.java
@@ -1,9 +1,26 @@
 package com.project.backend.services;
 
+import com.project.backend.entities.Task;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TaskService {
+    public Task createTask(Task task) {
+    }
+
+    public List<Task> getAllTasks() {
+    }
+
+    public Task getTaskById(Long id) {
+    }
+
+    public Task updateTask(Long id, Task task) {
+    }
+
+    public void deleteTask(Long id) {
+    }
     //TaskService.java: Gestiona la lógica de negocio para las tareas, como la creación, asignación a usuarios,
     // actualización de estado y eliminación de tareas.
 }
