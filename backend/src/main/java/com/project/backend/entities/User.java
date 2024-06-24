294052f093327a77e904d930c6f018f80e8ee850
ClaudiaCalero <claudiaclrduro@gmail.com>
2024-06-24 17:42:46 +0200

    user entity update

--- backend/src/main/java/com/project/backend/entities/User.java
+++ backend/src/main/java/com/project/backend/entities/User.java
@@ -1,28 +1,36 @@
 package com.project.backend.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
     private String username;
     private String password;
     private boolean enabled;
 
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+
     public User() {
     }
 
-    public User(long id, String username, String password, boolean enabled) {
+    public User(long id, String username, String password, boolean enabled, Set<Role> roles) {
         this.id = id;
         this.username = username;
         this.password = password;
         this.enabled = enabled;
+        this.roles = roles;
     }
 
     public long getId() {
@@ -55,5 +63,13 @@ public class User {
 
     public void setEnabled(boolean enabled) {
         this.enabled = enabled;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
     }
 }
