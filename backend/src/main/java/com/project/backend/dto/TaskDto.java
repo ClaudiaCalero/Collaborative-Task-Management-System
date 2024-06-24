1118c7fd52b60d93ad18e451f865a21b9415922b
ClaudiaCalero <claudiaclrduro@gmail.com>
2024-06-24 17:35:26 +0200

    TaskDTO dto

--- backend/src/main/java/com/project/backend/dto/TaskDto.java
+++ backend/src/main/java/com/project/backend/dto/TaskDto.java
@@ -1,4 +1,51 @@
 package com.project.backend.dto;
 
+import java.util.Date;
+
 public class TaskDto {
+    private String title;
+    private String description;
+    private Date dueDate;
+    private Long projectId;
+    private Long userId;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public Long getProjectId() {
+        return projectId;
+    }
+
+    public void setProjectId(Long projectId) {
+        this.projectId = projectId;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
 }
